2bc9b921949d199bcf638357ab01fd92695633f2
Lloyd Africa <lloyd.africa@unboxedconsulting.com>
2016-07-08 10:43:00 +0200

    offline test

--- app.js
+++ app.js
@@ -247,6 +247,18 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     */
 
+    /* offline testing */
+
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.register('sw.js', {
+        scope: '/'
+      }).then(function(reg) {
+        console.log(reg);
+      }).catch(function(err) {
+        console.log(err);
+      });
+    }
+
 });
 
 
