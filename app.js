ef1f0f67942300be517c95ea49ffb598d927ab40
Lloyd Africa <lloyd.africa@unboxedconsulting.com>
2016-07-08 13:40:46 +0200

    forward slasher

--- app.js
+++ app.js
@@ -251,7 +251,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
     if ('serviceWorker' in navigator) {
       navigator.serviceWorker.register('sw.js', {
-        scope: '/021/'
+        scope: '/'
       }).then(function(reg) {
         console.log(reg);
       }).catch(function(err) {
