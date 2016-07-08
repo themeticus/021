a66b7371aa2b76b6ab8dd09e6ba37df9d55754c9
Lloyd Africa <lloyd.africa@unboxedconsulting.com>
2016-07-08 13:50:22 +0200

    rtfm vs copy-&-paste driven development

--- app.js
+++ app.js
@@ -250,7 +250,7 @@ document.addEventListener('DOMContentLoaded', function() {
     /* offline */
 
     if ('serviceWorker' in navigator) {
-      navigator.serviceWorker.register('sw.js', {
+      navigator.serviceWorker.register('https://themeticus.github.io/021/sw.js', {
         scope: 'https://themeticus.github.io/021/'
       }).then(function(reg) {
         console.log(reg);
