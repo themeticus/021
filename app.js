37e5dde5f36367d234ab374bc90301ba769b04ec
Lloyd Africa <lloyd.africa@unboxedconsulting.com>
2017-12-22 01:24:18 +0200

    prep ++

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
+var stories = document.getElementById('stories').getElementsByTagName('h1');
+var titles = document.createDocumentFragment();
 
+function loadTitleMenu() {  
 
+  for (var i = 0; i < stories.length; i++ ) { 
+    var newTitle = document.createElement('h1');
+    newTitle.innerText = stories[i].innerText;
+    titles.appendChild(newTitle);
+  }
+
+  document.getElementById('searchByTitle').appendChild(titles);
+
+}
