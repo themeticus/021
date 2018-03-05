d28f6971ecd3c4776686ea2138fd04d3330d12cb
Hugh Mosno <lloydafrica@gmail.com>
2018-03-05 20:43:35 +0200

    for the ouens

--- app.js
+++ app.js
@@ -10,5 +10,11 @@ function loadTitleMenu() {
   }
 
   document.getElementById('searchByTitle').appendChild(titles);
+  			
+}
 
+var editor = {
+	simple: 'data:text/html;charset=utf-8, <html contenteditable></code>',
+	base64: 'data:text/html;base64,PGh0bWwgY29udGVudGVkaXRhYmxlPg==',
+	matrix: 'data:text/html, <style>html,body{margin: 0; padding: 0;}</style><textarea style="font-size: 1.5em; line-height: 1.5em; background: %23000; color: %233a3; width: 100%; height: 100%; border: none; outline: none; margin: 0; padding: 90px;" autofocus placeholder="wake up Neo..." />'
 }
